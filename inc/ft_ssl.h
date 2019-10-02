b906e7b6eb01b956735f76dd31a887b68a320fe3
Amina MANSOUR <amansour@e1r1p8.42.fr>
2019-10-02 13:56:08 +0200

    norme fixed

--- inc/ft_ssl.h
+++ inc/ft_ssl.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ssl.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amansour <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/02 13:40:22 by amansour          #+#    #+#             */
+/*   Updated: 2019/10/02 13:40:38 by amansour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_SSL_H
 # define FT_SSL_H
 
@@ -24,6 +36,6 @@ char					*ft_strjoin_s(char const *s1, char const *s2, t_u64 l);
 void					ft_putstr_size(char *s, size_t size);
 void					*ft_memcpy_64(void *dest, const void *src, t_u64 len);
 void					*ft_calloc(t_u64 nmemb, size_t size);
-int		                ft_strcmp(char const *str1, char const *str2);
+int						ft_strcmp(char const *str1, char const *str2);
 
 #endif
--- inc/ft_ssl_crypt.h
+++ inc/ft_ssl_crypt.h
@@ -1,12 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ssl_crypt.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amansour <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/02 13:40:50 by amansour          #+#    #+#             */
+/*   Updated: 2019/10/02 13:52:59 by amansour         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_SSL_CRYPT_H
 # define FT_SSL_CRYPT_H
 
 # include "ft_ssl.h"
-# include <sys/random.h>
 
 # define KY64 "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
 # define BASE64 KY64
 # define BASE16			"0123456789ABCDEF"
+
 typedef uint8_t			t_u8;
 typedef uint32_t		t_u32;
 typedef uint64_t		t_u64;
@@ -37,7 +49,7 @@ typedef struct			s_des_flags
 	char				*iv;
 	char				*tmp;
 	char				*mode;
-	char				a;		
+	char				a;
 }						t_des_flags;
 
 typedef struct			s_key
@@ -54,44 +66,46 @@ typedef struct			s_block
 
 typedef struct			s_array
 {
-	int 				row;
-	int 				col;
-    int                 val;
+	int					row;
+	int					col;
+	int					val;
 }						t_array;
 
-
 void					base64(int argc, char **argv);
-int						base64_encode(char *in, t_u64 len, char **out, t_u64 *outlen);
-int						base64_decode(char *in, t_u64 len, char	**out, t_u64 *outlen);
+int						base64_encode(char *in, t_u64 len, char **out,\
+						t_u64 *ol);
+int						base64_decode(char *in, t_u64 len, char **out,\
+						t_u64 *ol);
 void					usage_base64(void);
 void					des(int argc, char **argv);
 void					usage_des(void);
-int         			ft_generate_iv_keys(t_des_flags *f, char **str, int *len);
-int     				hex_expr(char *hex);
+int						ft_generate_iv_keys(t_des_flags *f, char **s, int *l);
+int						hex_expr(char *hex);
 char					*hex2dec(char *str, int size);
 char					*hex2bin(char *s);
 char					*bin2dec(char *s);
 char					*dec2bin(char *s);
-char 					*bin2hex(char *s);
+char					*bin2hex(char *s);
 char					*encrypt_des(char *pt, char *key, t_des_flags *f);
 char					*decrypt_des(char *ct, char *key, t_des_flags *f);
 int						decrypt_base64(char **str, int len, t_des_flags *flags);
 int						decrypt_salt(t_des_flags *f, char **s, int *len);
-int     				crypt_function(char *round_key, t_block *bk, int i);
-int         			init_block(char *pt, t_block *block, char *iv);
-char        			*final_text(t_block *bk, t_key k);
-int             		s_boxes(char *x, char **op);
-int    					pretreat_des(t_des_flags *flags, char **str, int *len);
-void					encrypt_base64(t_des_flags *flags, char *str, int len);
+int						crypt_function(char *round_key, t_block *bk, int i);
+int						init_block(char *pt, t_block *block, char *iv);
+char					*final_text(t_block *bk, t_key k);
+int						s_boxes(char *x, char **op);
+int						pretreat_des(t_des_flags *flags, char **s, int *l);
+void					encrypt_base64(t_des_flags *flags, char *s, int l);
 void					crypt_des(t_des_flags *flags, char *str, int len);
 /*
 **	tools function
 */
 char					*xor_function(char *a, char *b);
-char					*permute(char *key, int* arr, int n);
+char					*permute(char *key, int *arr, int n);
 char					*shift_left(char *k, int shifts);
 int						split(char *str, char **left, char **right, int size);
-char					*round_key_function(char **leftkey, char **rightkey, int shift);
+char					*round_key_function(char **left, char **right,\
+						int shift);
 int						keygen(char **key);
 char					*short_key(char *key);
 #endif
